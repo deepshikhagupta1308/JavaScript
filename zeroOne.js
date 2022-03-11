5719e179a94d9c797cd69b61c5b0959bd75096a2
deepshikhagupta <deepshikhagupta131995@gmail.com>
2022-03-11 20:54:54 +0530

    files added

---
+++ zeroOne.js
@@ -0,0 +1,11 @@
+//Approach1
+    function zeroOne(input){
+        return 1-input;
+    }
+  zeroOne(1);
+
+//Approach2
+    function zeroOne (input){
+        return (input+1)%2;
+    }
+ zeroOne(1);
