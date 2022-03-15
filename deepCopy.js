5aecc1608909228b2133846cd96f08e235796106
deepshikhagupta <deepshikhagupta131995@gmail.com>
2022-03-15 17:48:57 +0530

    changes added

--- deepCopy.js
+++ deepCopy.js
@@ -1,37 +1,31 @@
 const object = {
-    name: "Deepshikha",
-    qualification:"Graduation",
-    city:"Indore",
-    details : {
-        address:"168",
-        phone:{
-            number1:"1234567892",
-            number2:"9876540321"
-        }
-    }
-  };
-  
-  function deepCopy(object){
-    const object1 = {};
-    
-    for (const property in object) {
+  name: "Deepshikha",
+  qualification: "Graduation",
+  city: "Indore",
+  details: {
+    address: "168",
+    phone: {
+      number1: "1234567892",
+      number2: "9876540321",
+    },
+  },
+};
 
+function deepCopy(object) {
+  const copiedObject = {};
+
+  for (const property in object) {
     if (typeof object[property] !== "object") {
-      
-      object1[property] = object[property];
-    
-    }
-    else if (typeof object[property] === "object") {
-      
-      object1[property] = deepCopy(object[property]);
-    
+      copiedObject[property] = object[property];
+    } else if (typeof object[property] === "object") {
+      copiedObject[property] = deepCopy(object[property]);
     }
   }
-  
-    return object1;
-  };
-  
-  object2 = deepCopy(object);
-  object2.details.address = "789";
-  console.log(object2);
-  console.log(object.details.address);
+
+  return copiedObject;
+}
+
+newObject = deepCopy(object);
+newObject.details.address = "789";
+console.log(newObject);
+console.log(object.details.address);
