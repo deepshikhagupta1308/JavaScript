e8cf21476e27ebac5fbe7a798e3b183041fef2aa
deepshikhagupta <deepshikhagupta131995@gmail.com>
2022-03-18 20:51:42 +0530

    new file added

---
+++ problem1.js
@@ -0,0 +1,27 @@
+/**
+ * @description Method to perform airthmetic operation
+ * @param {Number} a
+ * @param {Number} b
+ * @param {String} operator
+ * @returns {Number} *
+ */
+ function calculatorV1(a, b, operator) {
+    if (operator === "add") {
+     return a + b;
+    } else if (operator === "subtract") {
+     return  a - b;
+    } else if (operator === "divide") {
+     return a / b;
+    } else if (operator === "multiply") {
+     return a * b;
+    } else {
+      console.log("Not a valid operator");
+    }
+  }
+  
+  console.log(calculatorV1(1, 3, "add")); // 4
+  console.log(calculatorV1(10, 2, "subtract")); // 8
+  console.log(calculatorV1(10, 5, "divide")); // 2
+  console.log(calculatorV1(10, 3, "multiply")); // 30
+  console.log(calculatorV1(10, 3, "modulas")); // Not a valid operator 
+  
